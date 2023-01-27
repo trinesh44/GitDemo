1b225a50beab47181568b33acf8dc4e5dd784629
trinesh <trinesh@01.com>
2023-01-27 10:36:04 +0530

    jfj

---
+++ src/src/Arthematic.java
@@ -0,0 +1,17 @@
+package src;// Java code to illustrate Subtraction operator
+
+class Arthematic {
+    public static void main(String[] args)
+    {
+        // initializing variables
+        int num1 = 20, num2 = 10, sub = 0;
+
+        // Displaying num1 and num2
+        System.out.println("num1 = " + num1);
+        System.out.println("num2 = " + num2);
+
+        // subtracting num1 and num2
+        sub = num1 - num2;
+        System.out.println("Subtraction = " + sub);
+    }
+}
